ed7bb207c69b6cfce1a0521848206dac47d94e43
Prd46 <prd46@drexel.edu>
2023-02-21 00:02:17 -0500

    Padding change

--- wp-content/themes/firstTheme/footer.php
+++ wp-content/themes/firstTheme/footer.php
@@ -1,5 +1,5 @@
 <footer>
-    <p>Copyright &copy; Business Brand <?php echo date('Y'); ?>, all rights reserved.</p>
+    <p class="copyright">Copyright &copy; Business Brand <?php echo date('Y'); ?>, all rights reserved.</p>
     <?php wp_nav_menu(['theme_location' => 'footer-menu']); ?>
 </footer>
 <?php wp_footer();?>
