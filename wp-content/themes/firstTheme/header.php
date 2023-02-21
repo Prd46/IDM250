d6b850e8ae40d7b11d0635000a77926ce5616ce7
Prd46 <prd46@drexel.edu>
2023-02-20 23:55:13 -0500

    Finalizing changes

--- wp-content/themes/firstTheme/header.php
+++ wp-content/themes/firstTheme/header.php
@@ -8,10 +8,20 @@
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?> >
-
+<header>
+    <div class="header-internal-wrapper">
+    <div class="header-identity-wrapper">
+    <div class="header-image">
+    <img class="header-image-file" src="<?php echo get_template_directory_uri(); ?>/dist/images/kthump.png" alt="Logo">
+    </div>
+    <h3>Artist's Name</h3>
+</div>  
+<div class="header-menu-links">
 <?php wp_nav_menu(['theme_location' => 'primary-menu']); 
-
 ?>
+</div>
+</div>
+</header>
 <?php
 wp_body_open();
 ?>
