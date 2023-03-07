487dca6b456fe971da5918e045b5d6befb955f6c
Prd46 <prd46@drexel.edu>
2023-03-06 21:28:09 -0500

    404 Page

---
+++ wp-content/themes/firstTheme/404.php
@@ -0,0 +1,12 @@
+<?php get_header() ?>
+<h1><?php echo get_the_title(); ?></h1>
+
+<div class="404-text-box">
+    <h1 class="404-header-text">404</h1>
+    <h3 class="404-message">Whatever page you're looking for, it's not here.</h3>
+    <div class="404-return-home-button">
+        <a href="" class="404-return-home-link">Head Home</a>
+    <div>
+</div>
+
+<?php get_footer() ?>
--- wp-content/themes/firstTheme/single.php
+++ wp-content/themes/firstTheme/single.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 <h1><?php get_the_title(); ?></h1>
 <div><?php get_the_excerpt(); ?></div>
-
+<div><?php echo get_the_date();?> | </div>
 <h2>Categories for this post</h2>
 <?php
 $currentPostId= get_the_id();
