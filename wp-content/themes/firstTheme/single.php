a6b77a1070f7205575267f627535d1bb27147f5b
Prd46 <prd46@drexel.edu>
2023-03-07 17:06:00 -0500

    Categories and date

--- wp-content/themes/firstTheme/single.php
+++ wp-content/themes/firstTheme/single.php
@@ -1,19 +1,27 @@
 <?php get_header(); ?>
-<h1><?php get_the_title(); ?></h1>
-<div><?php get_the_excerpt(); ?></div>
-<div><?php echo get_the_date();?> | </div>
-<h2>Categories for this post</h2>
+<h1 class="page-title-custom"><?php echo get_the_title(); ?></h1>
+<div class="page-excerpt"><?php echo get_the_excerpt(); ?></div>
+<div class="page-date"><?php echo get_the_date();?></div>
+<h4>Categories for this post</h4>
+<div class="categories-box">
 <?php
 $currentPostId= get_the_id();
 
 $terms = get_the_terms($currentPostId, 'category');
 
 if ($terms) {
-    foreach ($terms as $term){
-        echo "<span>{$term->name}</span>";
+    $total_category_count = count($terms);
+    echo '<div class="flex flex-wrap" data-component="blog-categories-list">';
+    foreach ($terms as $index => $term) {
+        $taxonomy_url = get_term_link($term->term_id);
+        // Only show comma if not last item in loop
+        // Commenting out this comma logic since my design doesn't need it but leaving it here for reference
+        $comma = $index < $total_category_count - 1 ? '| ' : '';
+        echo "<span><a class='category-link' href='{$taxonomy_url}'>{$term->name}</a> {$comma}<span>";
     }
+    echo '</div>';
 }
 ?>
-
+</div>
 <?php get_template_part('components/content'); ?>
 <?php get_footer(); ?>
