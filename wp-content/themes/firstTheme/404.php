74d929ade24bd25e28c7d9b2e26d618b75d11deb
Prd46 <prd46@drexel.edu>
2023-03-07 16:24:09 -0500

    404 Styles

--- wp-content/themes/firstTheme/404.php
+++ wp-content/themes/firstTheme/404.php
@@ -1,12 +1,14 @@
 <?php get_header() ?>
 <h1><?php echo get_the_title(); ?></h1>
 
-<div class="404-text-box">
-    <h1 class="404-header-text">404</h1>
-    <h3 class="404-message">Whatever page you're looking for, it's not here.</h3>
-    <div class="404-return-home-button">
-        <a href="" class="404-return-home-link">Head Home</a>
-    <div>
-</div>
+<div class="text-box-404">
+    <h1 class="header-text-404">404</h1>
+    <h3 class="message-404">Whatever page you're looking for, it's not here.</h3>
+    <a href="">
+    <div class="return-home-button-404">
+        Head Home
+    </div>
+</a>
+</div> 
 
 <?php get_footer() ?>
