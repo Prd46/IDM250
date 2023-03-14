6769aedd038ba7f0b3c89372ea36949bd6989367
Prd46 <prd46@drexel.edu>
2023-03-13 22:34:15 -0400

    Adding sidebar

--- wp-content/themes/firstTheme/new-template-page.php
+++ wp-content/themes/firstTheme/new-template-page.php
@@ -1,3 +1,8 @@
 <?php
 
 /* Template Name: Sidebar temp */
+ get_header(); ?>
+<h1 class="page-title-custom"><?php echo get_the_title(); ?></h1>
+<?php get_template_part('components/content'); ?>
+<?php get_template_part('components/sidebar'); ?>
+<?php get_footer(); ?>
