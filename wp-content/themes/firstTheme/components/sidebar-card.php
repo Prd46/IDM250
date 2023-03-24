af944d1481f8472a8df6187fbbb3a3bb14fff8a3
Prd46 <prd46@drexel.edu>
2023-03-24 00:00:51 -0400

    Some more styling

--- wp-content/themes/firstTheme/components/sidebar-card.php
+++ wp-content/themes/firstTheme/components/sidebar-card.php
@@ -1,8 +1,5 @@
 <a href="<?php echo get_the_permalink(); ?>">
-<div class="card-box">
-    <div class="card-caption">
-    <h3> <?php echo get_the_title(); ?></h3>
-  </div>
+<div class="sidebar-card-box">
   <div class="card-image">
   <?php if (get_the_post_thumbnail_url()) {
       echo "<div class='w-100 mr-8'>
--- wp-content/themes/firstTheme/components/sidebar.php
+++ wp-content/themes/firstTheme/components/sidebar.php
@@ -1,5 +1,4 @@
 <div class="sidebar">
     <h4>Other pages</h4>
     <?php wp_nav_menu(['theme_location' => 'sidebar-menu']); ?>
-    <h4>Posts</h4>
 </div>
