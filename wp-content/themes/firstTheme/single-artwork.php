3c72d23b1de6b80301afba1c48e5981d184d19be
Prd46 <prd46@drexel.edu>
2023-03-23 21:21:41 -0400

    Functional archive page

--- wp-content/themes/firstTheme/single-artwork.php
+++ wp-content/themes/firstTheme/single-artwork.php
@@ -9,9 +9,16 @@ $currentPostId= get_the_id();
 $terms = get_the_terms($currentPostId, 'category');
 
 if ($terms) {
-    foreach ($terms as $term){
-        echo "<span>{$term->name}</span>";
+    $total_category_count = count($terms);
+    echo '<div class="flex flex-wrap" data-component="blog-categories-list">';
+    foreach ($terms as $index => $term) {
+        $taxonomy_url = get_term_link($term->term_id);
+        // Only show comma if not last item in loop
+        // Commenting out this comma logic since my design doesn't need it but leaving it here for reference
+        $comma = $index < $total_category_count - 1 ? '| ' : '';
+        echo "<span><a class='category-link' href='{$taxonomy_url}'>{$term->name}</a> {$comma}<span>";
     }
+    echo '</div>';
 }
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
